189b01a31f5a953381a3d2f3227bbbe7984475c0
Stepan Skryabin <20664125+zurg3@users.noreply.github.com>
2020-06-03 17:48:13 +0300

    Edited hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
-#if defined(WIN32) || defined(_WIN32)
-  #include <windows.h>
-#endif
 using namespace std;
 
 int main() {
-  int a = 64 * 64;
-  
   cout << "Hello, World!" << endl;
-  
-  cout << a << endl;
   
   return 0;
 }
